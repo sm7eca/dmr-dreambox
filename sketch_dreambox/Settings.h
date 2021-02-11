86f92abe840fc905d7b5de6db2d46b02f0b7dffb
sm7eca <svangen@gmail.com>
2021-02-11 11:02:45 +0100

    Revert "Merge pull request #32 from sm7eca/feat/settings-add-wifi"
    This reverts commit 8298cedb5bbdebbef125573e1ead8cabe860583d, reversing
    changes made to 9c2265cccb8d95d3b56ee75ff4dba8fe96f9ff17.

--- sketch_dreambox/Settings.h
+++ sketch_dreambox/Settings.h
@@ -2,12 +2,9 @@
 #ifndef SETTINGS_H
 #define SETTINGS_H
 
-#define SETTINGS_WIFI_SSID_LEN 32
-#define SETTINGS_WIFI_PASSWD_LEN 16
-
 typedef struct _WifiSettingS {
-  char ssid[SETTINGS_WIFI_SSID_LEN + 1];
-  char passwd[SETTINGS_WIFI_PASSWD_LEN + 1];
+  char ssid[33];
+  char passwd[17];
 } WifiSettingS;
 
 typedef struct _TalkGroupS {
@@ -26,8 +23,6 @@ typedef struct _RepeaterS {
   TalkGroupS  groups[10];
 } RepeaterS;
 
-#define SETTINGS_MAX_WIFI_AP 4
-
 
 typedef struct _RepeaterConfigS {
   RepeaterS   repeater[50];
@@ -36,7 +31,7 @@ typedef struct _RepeaterConfigS {
 
 typedef struct _DmrSettingsS {
   int           version;
-  WifiSettingS  wifisettings[SETTINGS_MAX_WIFI_AP];
+  WifiSettingS  wifisettings[4];
   uint8_t       audioLevel;       //  1-9; default = 8
   uint8_t       micLevel;         //  0-15, mic gain setting
   char          callSign[12];     // callsign, max 12 chars
@@ -67,8 +62,5 @@ void settingsWrite(DmrSettingsS* dmrSettings);
 
 // read settings from EEPROM into dmrSettings
 void settingsRead(DmrSettingsS* dmrSettings);
-
-// write wifi settings into to a distinct slot
-void settingsAddWifiAp(DmrSettingsS* dmrSettings, WifiSettingS* wifiAp, int slot);
 
 #endif /* SETTINGS_H */
