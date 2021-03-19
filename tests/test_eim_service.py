a237b412c1116f34eab008c88eb71c9bd7e9bf88
Sebastian Mangelsen (Github) <github.mangelsen.se>
2021-03-19 22:02:36 +0100

    EIM - add number of users in DB to system info
    - add new member "user" to JSON structure returned for
      GET => /system/info
    - provide function tests for new member in system/info
      return JSON data
    - ensure that we set both "repeater" and "user" to 0
      if we won't be able to find any collection (initialized
      database will take some time to populate the data)
    - prepare the function tests for being reused when
      deploying to production, introduces a host name that
      can be passed when running the function test against a 
      certain host

--- tests/test_eim_service.py
+++ tests/test_eim_service.py
@@ -4,15 +4,21 @@ import requests
 from time import sleep
 from typing import Dict
 
-BASE_URL = "http://localhost/api/v1"
+API_ROOT = "api/v1"
 
 
-def test_sysinfo():
-	response = requests.get(url=f"{BASE_URL}/system/info")
+def test_sysinfo(hostname):
+	url = f"http://{hostname}/{API_ROOT}/system/info"
+	response = requests.get(url=url)
 	assert response.status_code == 200
+	assert response.json()
+	data = response.json()
+	assert "user" in data.keys()
+	assert "repeater" in data.keys()
 
-def test_docs():
-	response = requests.get(url=f"{BASE_URL}/docs")
+def test_docs(hostname):
+	url = f"http://{hostname}/{API_ROOT}/docs"
+	response = requests.get(url=url)
 	assert response.status_code == 200
 
 
@@ -27,8 +33,9 @@ def test_docs():
 		"invalid master_id"
 	]
 )
-def test_repeater_master(master_id: int, expected_status: int, expected_len: int):
-	response = requests.get(url=f"{BASE_URL}/repeater/master/{master_id}?limit=5&skip=0")
+def test_repeater_master(master_id: int, expected_status: int, expected_len: int, hostname):
+	url = f"http://{hostname}/{API_ROOT}/repeater/master/{master_id}?limit=5&skip=0"
+	response = requests.get(url=url)
 	assert response.status_code == expected_status
 	if response.status_code == 200:
 		assert len(response.json()) == expected_len
@@ -47,18 +54,21 @@ def test_repeater_master(master_id: int, expected_status: int, expected_len: int
 		"callsign_regex"
 	]
 )
-def test_repeater_callsign(call_sign: str, expected_status: int, expected_len: int):
-	response = requests.get(url=f"{BASE_URL}/repeater/callsign/{call_sign}")
+def test_repeater_callsign(call_sign: str, expected_status: int, expected_len: int, hostname):
+	url = f"http://{hostname}/{API_ROOT}/repeater/callsign/{call_sign}"
+	response = requests.get(url=url)
 	assert response.status_code == expected_status
 	if response.status_code == 200:
 		assert len(response.json()) == expected_len
 
-def test_invalid_api_endpoint_exception_covered():
-	response = requests.get(url=f"{BASE_URL}/repeater/dmr/A87987d9a8s7d9?lim=12")
+def test_invalid_api_endpoint_exception_covered(hostname):
+	url = f"http://{hostname}/{API_ROOT}/repeater/dmr/A87987d9a8s7d9?lim=12"
+	response = requests.get(url)
 	assert response.status_code == 404
 
-def test_no_repeater_dmr_id():
-	response = requests.get(url=f"{BASE_URL}/repeater/dmr/2407099")
+def test_no_repeater_dmr_id(hostname):
+	url = f"http://{hostname}/{API_ROOT}/repeater/dmr/2407099"
+	response = requests.get(url)
 	assert response.status_code == 404
 
 @pytest.mark.parametrize(
@@ -76,8 +86,9 @@ def test_no_repeater_dmr_id():
 		"check_hotspot"
 	]
 )
-def test_repeater_dmrid(dmr_id: int, expected_status: int, expected_len: int):
-	response = requests.get(url=f"{BASE_URL}/dmr/{dmr_id}")
+def test_repeater_dmrid(dmr_id: int, expected_status: int, expected_len: int, hostname):
+	url = f"http://{hostname}/{API_ROOT}/dmr/{dmr_id}"
+	response = requests.get(url)
 	assert response.status_code == expected_status
 	if response.status_code == 200:
 		assert not isinstance(response.json(), list)
@@ -106,7 +117,8 @@ def test_repeater_dmrid(dmr_id: int, expected_status: int, expected_len: int):
 
 
 def test_repeater_location():
-	response = requests.get(url=f"{BASE_URL}/repeater/location?city=Gothenburg&country=SE&distance=40")
+	url = f"http://{hostname}/{API_ROOT}/repeater/location?city=Gothenburg&country=SE&distance=40"
+	response = requests.get(url)
 	assert response.status_code == 501
 
 
@@ -121,23 +133,25 @@ def test_repeater_location():
 		"valid callsign"
 	]
 )
-def test_hotspot(callsign: str, expected_status: int):
+def test_hotspot(callsign: str, expected_status: int, hostname):
 	"""
 	Given a valid callsign, we expected that will receive a number of hotspots
 	"""
-	response = requests.get(url=f"{BASE_URL}/hotspot/callsign/{callsign}")
+	url = f"http://{hostname}/{API_ROOT}/hotspot/callsign/{callsign}"
+	response = requests.get(url)
 	assert response.status_code == expected_status
 
 
-def test_redirect_root():
+def test_redirect_root(hostname):
 	"""Given a GET request sent towards root, we expect to be redirected to /docs"""
 
 	# call the UUT
-	response = requests.get(url=BASE_URL, allow_redirects=True)
+	url = f"http://{hostname}/{API_ROOT}"
+	response = requests.get(url=url, allow_redirects=True)
 
 	# assert results
 	assert response.status_code == 200
-	assert response.url == f"{BASE_URL}/docs"
+	assert response.url == f"{url}/docs"
 	assert response.history[0].is_redirect
 	assert response.history[0].status_code == 301
 
@@ -155,13 +169,13 @@ def test_redirect_root():
 		"falkenberg+99,distance-invalid"
 	]
 )
-def test_repeater_location(longitude, latitude, distance, limit, expected_status):
+def test_repeater_location(longitude, latitude, distance, limit, expected_status, hostname):
 	"""
 	Given a location in Falkenberg, there is no DMR enabled
 	repeater in that area
 	"""
-
-	req_url = f"{BASE_URL}/repeater/location" + \
+	url = f"http://{hostname}/{API_ROOT}"
+	req_url = f"{url}/repeater/location" + \
 				 f"?longitude={longitude}&latitude={latitude}&distance={distance}"
 
 	if limit:
@@ -187,12 +201,12 @@ def test_repeater_location(longitude, latitude, distance, limit, expected_status
 		"invalid => 422"
 	]
 )
-def test_user_dmrid(dmr_id, expected_status):
+def test_user_dmrid(dmr_id, expected_status, hostname):
 	"""
 	Given a valid user DMR ID, I expect the UUT to return a User response
 	"""
 	sleep(2)
-	req_url = f"{BASE_URL}/user/{dmr_id}"
+	req_url = f"http://{hostname}/{API_ROOT}/user/{dmr_id}"
 
 	response = requests.get(url=req_url)
 
@@ -202,14 +216,14 @@ def test_user_dmrid(dmr_id, expected_status):
 		data = response.json()
 		assert data['call_sign'] == "SM0TSC"
 
-def test_request_limiter():
+def test_request_limiter(hostname):
 	"""
 	Given that we have configured the NGINX proxy with request limiting
 	we should never receive 503, instead requests are delayed before served
 	"""
 	sleep(4)
 	counter = 20
-	url = f"{BASE_URL}/system/info"
+	url = f"http://{hostname}/{API_ROOT}/system/info"
 	while counter > 0:
 		counter -= 1
 		response = requests.get(url=url)
